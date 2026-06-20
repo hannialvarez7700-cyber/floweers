4b2ad5b9f4c2d746d874f7371e3cc66806664da3
hannialvarez7700-cyber <hannialvarez7700@gmail.com>
2026-06-19 22:32:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirCarta(){
+
+    document.getElementById("mensaje").innerHTML =
+
+    `🌼 Gracias por tu linda amistad.<br><br>
+
+    Mi linda geni, me da alegría que seas parte de mi vida, a veces ni siento la diferencia de edad que hay entre nosotras, porque me siento tan comoda contigo. Me emociona estar en esta nueva etapa tuya, se que en un futuro serás esa gran odontóloga. TE AMOO 🍓`;
+}
